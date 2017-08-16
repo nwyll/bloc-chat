2a735ffc9837a4a1676023bee608812c7bd1cfd9
Natalie Wyll <nataliewyll@yahoo.com>
2017-08-15 23:19:26 -0500

    checkpoint-3: adds modal to create a new room

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,17 +3,23 @@
   function Room($firebaseArray) {
     //create an empty Room object
     var Room = {};
-    //creates a reference to our rooms database 
+    //creates a reference to our rooms database
     var ref = firebase.database().ref().child("rooms");
     //rooms uses the $firebaseArray service to make ref an array
     var rooms = $firebaseArray(ref);
+
     //append rooms to the Room object as Room.all
     Room.all = rooms;
-    
+
+    //creating a add method to insulate the firebase $add method
+    Room.add = function(room) {
+      rooms.$add(room)
+    };
+
     //return the Room object that now contains an array of the rooms in the database
     return Room;
   }
-  
+
   angular
     .module('blocChat')
     .factory('Room', ['$firebaseArray', Room])
