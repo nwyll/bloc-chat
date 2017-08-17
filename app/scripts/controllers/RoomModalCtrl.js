2aa12bca924405ace47f680aaba8d709d2567fb1
Natalie Wyll <nataliewyll@yahoo.com>
2017-08-17 13:12:10 -0500

    checkpoint-5: added new username modal and renamed old modal to room modal

--- app/scripts/controllers/RoomModalCtrl.js
+++ app/scripts/controllers/RoomModalCtrl.js
@@ -1,23 +1,23 @@
 (function() {
     /**
-    * @function ModalCtrl
+    * @function RoomModalCtrl
     * @desc Opens the modal to create a new room.
     * @param $uibModal
     */
-    function ModalCtrl($uibModal) {
+    function RoomModalCtrl($uibModal) {
       /**
       * @function open
       * @desc Opens the modal and creates a modal instance.
       */
       this.open = function() {
         var modalInstance = $uibModal.open({
-          templateUrl: '/templates/modal.html',
-          controller: 'ModalInstanceCtrl as modalInstance'
+          templateUrl: '/templates/roomModal.html',
+          controller: 'RoomModalInstanceCtrl as roomModalInstance'
         })
       }
     }
 
     angular
         .module('blocChat')
-        .controller('ModalCtrl', ['$uibModal', ModalCtrl]);
+        .controller('RoomModalCtrl', ['$uibModal', RoomModalCtrl]);
 })();
